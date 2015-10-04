ff892dbdf9745088787d76ae945d30d565ce0102
Steve Anderson <phliver@mooncitylabs.org>
2015-10-04 04:18:54 -0500

    remove menu links. added site url. changed theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,18 +4,20 @@ from __future__ import unicode_literals
 
 AUTHOR = u'stevenix'
 SITENAME = u'Siouxland Linux Users Group'
-SITEURL = ''
+SITEURL = 'http://sluglinux.org'
 DELETE_OUTPUT_DIRECTORY = (True)
 
 PATH = 'content'
 
 TIMEZONE = 'America/Chicago'
+DEFAULT_DATE_FORMAT = '%b %d %Y'
 
 RELATIVE_URLS = (True)
 STATIC_PATHS = (['images', 'extra/favicon.ico', 'extra/robots.txt'])
 DISPLAY_CATEGORIES_ON_MENU = False
 DISPLAY_PAGES_ON_MENU = False
-THEME = './themes/bootstrap'
+#THEME = './themes/bootstrap'
+THEME = './themes/new-bootstrap2'
 MENUITEMS = (
         ('meetings', '/pages/meetings.html'),
         ('contact', '/pages/contact.html'),
@@ -29,18 +31,19 @@ CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
+TAGS_SAVE_AS = 'tags.html'
 
 # Blogroll
-LINKS =  (
-    ('Mailing List', 'https://groups.google.com/forum/#!forum/sluglinux'),
-    ('Wiki', 'http://www.sluglinux.org/wiki/Main_Page'),
-)
+#LINKS =  (
+#    ('Mailing List', 'https://groups.google.com/forum/#!forum/sluglinux'),
+#    ('Wiki', 'http://www.sluglinux.org/wiki/Main_Page'),
+#)
 
 # Social widget
-SOCIAL = (
-    ('Twitter', 'http://twitter.com/sluglinux'),
-    ('Feeds', 'http://sluglinux.org/feeds/atom.xml'),
-)
+#SOCIAL = (
+#    ('Twitter', 'http://twitter.com/sluglinux'),
+#    ('Feeds', 'http://sluglinux.org/feeds/atom.xml'),
+#)
 
 DEFAULT_PAGINATION = 5
 
