9ca52284add9bf96fce4572b0577aad20d8f2ba5
Dan Christensen <opello@opello.org>
2023-02-15 23:21:24 -0600

    Add Docker infrastructure to support Jekyll

---
+++ scripts/docker-run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+docker run \
+    --rm \
+    --interactive \
+    --tty \
+    --volume $PWD:/site \
+    --workdir /site \
+    --publish 4000:4000/tcp \
+    --publish 35729:35729/tcp \
+    --name sluglinux-build \
+    sluglinux-build:latest \
+    jekyll serve --destination /tmp/site --watch --host 0.0.0.0 --livereload --incremental
